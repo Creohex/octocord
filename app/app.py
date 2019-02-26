a4ea7d4615fed4be52e09acd60ee009a3401b433
Creohex <paradise.source@gmail.com>
2019-02-26 21:06:40 +0300

    hook functionality, db changes, other

--- app/app.py
+++ app/app.py
@@ -57,6 +57,7 @@ def check_headers(req):
         raise Exception("Error: no user with such id (%s)" % req.headers['UUID'])
     elif user.uuid_secret != req.headers['UUID_SECRET']:
         raise Exception("Error: secret mismatch")
+    return user
 
 
 # db operations:
@@ -96,36 +97,40 @@ def test():
 @app.route("/register", methods=['POST'])
 def register_user():
     """ register new user """
-    # check payload / existing users
-    payload = request.get_json()
-    if "username" not in payload.keys():
-        raise Exception("'username' field required in payload")
-    new_username = payload['username']
-    users = User.get_users()
-    if next((_ for _ in users if _.name == new_username), None) is not None:
-        raise Exception("User with this name already exists")
-    
-    # generate id/secret
-    new_user = User(uuid.uuid4().hex, new_username, uuid.uuid4().hex)
-
-    # update db
-    query_db_commit("INSERT INTO user (id, name, secret) VALUES (UNHEX('%s'), '%s', UNHEX('%s'))"
-                    % (new_user.uuid, new_user.name, new_user.uuid_secret))
-
-    return json.dumps(new_user.as_json())
+    return json.dumps(User.add(payload).as_json())
 
 @app.route("/hook")
 def hook_list():
     check_headers(request)
-    return json.dumps({"existing hooks": Hook.get_hooks(request.headers['UUID'])})
+    return json.dumps({"hooks": [_.as_json() for _ in Hook.get_hooks(request.headers['UUID'])]})
 
 @app.route("/hook/add", methods=['POST'])
 def hook_add():
-    pass
+    user = check_headers(request)
+    user.add_hook(request.get_json())
+    return json.dumps({"result": "ok"})
+
+@app.route("/hook/<hook_uuid>", methods=['DELETE'])
+def hook_del(hook_uuid):
+    user = check_headers(request)
+    hooks = Hook.get_hooks(user.uuid)
+    hook = next((_ for _ in hooks if _.id == hook_uuid), None)
+    if hook is not None:
+        hook.delete()
+        return "deleted"
+    else:
+        raise Exception("No hook with this id exists")
 
 @app.route("/hook/<hook_uuid>", methods=['POST'])
 def hook_post(hook_uuid):
-    pass
+    user = check_headers(request)
+    hook = Hook.get_hook(hook_uuid)
+    payload = request.get_json()
+    try:
+        link = hook.get_link()
+        return(json.dumps(link))
+    except Exception as e:
+        raise Exception("hook_post() error: %s" % str(e))
 
 
 # objects:
@@ -139,22 +144,46 @@ class User():
         return("User '%s': %s / %s" % (self.name, self.uuid, self.uuid_secret))
 
     def as_json(self):
-        return {
-            "uuid": self.uuid,
-            "name": self.name,
-            "uuid_secret": self.uuid_secret
-        }
+        return {"uuid": self.uuid, "name": self.name, "uuid_secret": self.uuid_secret}
 
     def get_bots(self):
         pass
 
     def add_hook(self, payload):
-        pass
+        """ add new hook for this user """
+        Hook.add(payload, self.uuid)
+
+    def get_hooks(self):
+        return Hook.get_hooks(self.uuid)
+
+    @staticmethod
+    def add(payload):
+        # check payload / existing users
+        payload = request.get_json()
+        if "username" not in payload.keys():
+            raise Exception("'username' field required in payload")
+        new_username = payload['username']
+        users = User.get_users()
+        if next((_ for _ in users if _.name == new_username), None) is not None:
+            raise Exception("User with this name already exists")
+
+        # generate id/secret
+        new_user = User(uuid.uuid4().hex, new_username, uuid.uuid4().hex)
+
+        # update db
+        query_db_commit("INSERT INTO user (id, name, secret) VALUES ('%s', '%s', '%s')"
+                        % (new_user.uuid, new_user.name, new_user.uuid_secret))
+
+        return new_user
+
+    @staticmethod
+    def exists(uuid):
+        return query_db("SELECT * FROM user WHERE id = '%s'" % uuid).rowcount != 0
 
     @staticmethod
     def get_users():
         return [User(id.lower(), name, secret.lower()) for id, name, secret
-                in query_db("SELECT HEX(id), name, HEX(secret) FROM user")]
+                in query_db("SELECT id, name, secret FROM user")]
 
 
 
@@ -167,15 +196,14 @@ class Bot():
 
 
 class Hook():
-    def __init__(self, id, name, channel_id, token, avatar, guild_id, hook_id, owner):
+    def __init__(self, id, name, channel_id, token, avatar, guild_id, owner_id):
         self.id = id
         self.name = name
         self.channel_id = channel_id
         self.token = token
         self.avatar = avatar
         self.guild_id = guild_id
-        self.hook_id = hook_id
-        self.owner = owner
+        self.owner_id = owner_id
 
     def __str__(self):
         print("%s (%s)" % (self.name, self.id))
@@ -188,19 +216,65 @@ class Hook():
             "token": self.token,
             "avatar": self.avatar,
             "guild_id": self.guild_id,
-            "hook_id": self.hook_id,
-            "owner": self.owner
+            "owner_id": self.owner_id
         }
 
     def get_link(self):
-        return "https://discordapp.com/api/webhooks/%s/%s" % (self.hook_id, self.token)
+        return "https://discordapp.com/api/webhooks/%s/%s" % (self.id, self.token)
 
     @staticmethod
-    def get_hooks(uuid):
-        return [Hook(ID.lower(), name, channel_id, token, avatar, guild_id, hook_id, owner.lower())
-                for ID, name, channel_id, token, avatar, guild_id, hook_id, owner
-                in query_db("SELECT HEX(id), name, channel_id, token, avatar, guild_id, hook_id, HEX(owner) "
-                            "FROM hook WHERE owner = '%s'" % uuid)]
+    def add(payload, user_id):
+        # check user
+        if not User.exists(user_id):
+            raise Exception("Hook.add() exception: no such user exists")
+
+        # check payload for required parameters
+        missing_parameters = (False, [])
+        for field in Hook.required_fields():
+            if field not in payload.keys():
+                missing_parameters[0] = True
+                missing_parameters[1].append(field)
+        if missing_parameters[0]:
+            raise Exception("Error: missing parameters (%s)" % ', '.join(missing_parameters[1]))
+
+        # check if such hook already exists
+        if Hook.exists(payload['id']):
+            raise Exception("Hook with this id already exists in database")
+
+        # add hook object
+        id = uuid.uuid4().hex
+        query_db_commit("INSERT INTO hook (id, name, channel_id, token, avatar, guild_id, owner_id) "
+                        "VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s')"
+                        % (payload['id'], payload['name'], payload['channel_id'], payload['token'],
+                           payload['avatar'], payload['guild_id'], user_id))
+
+        return "ok"
+
+    def delete(self):
+        query_db_commit("DELETE FROM hook WHERE id = '%s'" % self.id)
+
+    @staticmethod
+    def exists(id):
+        """ check if hook with 'id' exists """
+        return query_db("SELECT * FROM hook WHERE id = '%s'" % id).rowcount != 0
+
+    @staticmethod
+    def required_fields():
+        return ['id', 'name', 'channel_id', 'token', 'avatar', 'guild_id']
+
+    @staticmethod
+    def get_hooks(owner_uuid):
+        return [Hook(ID.lower(), name, channel_id, token, avatar, guild_id, owner_id.lower())
+                for ID, name, channel_id, token, avatar, guild_id, owner_id
+                in query_db("SELECT id, name, channel_id, token, avatar, guild_id, owner_id "
+                            "FROM hook WHERE owner_id = '%s'" % owner_uuid)]
+
+    @staticmethod
+    def get_hook(uuid):
+        return next(Hook(id, name, channel_id, token, avatar, guild_id, owner_id)
+                    for id, name, channel_id, token, avatar, guild_id, owner_id
+                    in query_db("SELECT id, name, channel_id, token, avatar, guild_id, owner_id "
+                                "FROM hook WHERE id = '%s'" % uuid))
 
 # server start
 if __name__ == "__main__":
--- assets/db_init.sql
+++ assets/db_init.sql
@@ -1,32 +1,35 @@
 # Create database:
 CREATE DATABASE IF NOT EXISTS octocord;
 
+
 # Switch to octocord db:
 USE octocord;
 
+
 # Delete tables:
-DROP table IF EXISTS user;
 DROP table IF EXISTS bot;
 DROP table IF EXISTS hook;
+DROP table IF EXISTS user;
+
 
 # Create tables:
 CREATE TABLE user(
-  id BINARY(16) NOT NULL,
+  id VARCHAR(32) NOT NULL,
   name VARCHAR(40) NOT NULL DEFAULT '',
-  secret BINARY(16) NOT NULL,
+  secret VARCHAR(32) NOT NULL,
   
   PRIMARY KEY (id)
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ENGINE=InnoDB;
 
-CREATE TABLE bot(
-  id INT NOT NULL AUTO_INCREMENT,
-  # ...
-
-  PRIMARY KEY (id)
-) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ENGINE=InnoDB;
+#CREATE TABLE bot(
+#  id INT NOT NULL AUTO_INCREMENT,
+#  # ...
+#
+#  PRIMARY KEY (id)
+#) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ENGINE=InnoDB;
 
 CREATE TABLE hook(
-  id BINARY(16) NOT NULL,
+  id VARCHAR(32) NOT NULL,
 
   name VARCHAR(126) NOT NULL DEFAULT '',
   channel_id VARCHAR(255) NOT NULL DEFAULT '',
@@ -35,13 +38,13 @@ CREATE TABLE hook(
   guild_id VARCHAR(255) NOT NULL DEFAULT '',
   hook_id VARCHAR(255) NOT NULL DEFAULT '',
 
-  owner BINARY(16) NOT NULL,
+  owner_id VARCHAR(32) NOT NULL,
 
-  PRIMARY KEY (id),
-  FOREIGN KEY (owner) REFERENCES user(id)
+  PRIMARY KEY (id, owner_id),
+  FOREIGN KEY (owner_id) REFERENCES user(id)
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ENGINE=InnoDB;
 
 
 # Initialize default data
 INSERT INTO user (id, name, secret) VALUES
-(UNHEX('8fc3cc1961964d12b67494dac10a7d93'), 'creohex', UNHEX('4cadd7d38eda4dda8415720b871f0a2d'))
+('8fc3cc1961964d12b67494dac10a7d93', 'creohex', '4cadd7d38eda4dda8415720b871f0a2d')
